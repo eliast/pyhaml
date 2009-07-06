e6f8fe17490928cf3331f04249f36a02d8a83b0c
brad dunbar <brad@brad-laptop.(none)>
2009-07-06 18:27:47 -0400

    make clean: remove *.py from test/haml
    __init__.py: remove cruft...

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 clean:
 	find . -name *.pyc | xargs rm -f
 	rm -f parser.out
+	rm -f test/haml/*.py
--- pyhaml/__init__.py
+++ pyhaml/__init__.py
@@ -1,4 +1,1 @@
-#pyhaml package
-# author: brad dunbar, dunbarb2@gmail.com
-
 __all__ = ['haml', 'patch2', 'patch3']
